994f5f0d76b97a0d4426f7421d704d34a63f041a
instagramuser9091033708-cyber <instagramuser9091033708@gmail.com>
2026-03-21 14:52:38 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -21,3 +21,19 @@ function login() {
 if(localStorage.getItem("user") && window.location.pathname.includes("index.html")) {
     window.location.href = "dashboard.html";
 }
+// Show username
+if(document.getElementById("welcome")) {
+    let user = localStorage.getItem("user");
+    document.getElementById("welcome").innerText = "Welcome " + user + " 👋";
+}
+
+// Open page (temporary)
+function openPage(name) {
+    alert(name + " page coming soon 🚀");
+}
+
+// Logout
+function logout() {
+    localStorage.clear();
+    window.location.href = "index.html";
+}
